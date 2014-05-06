cb01e16b88421a7a77d856c178f7706226ad5d2a
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2014-05-06 18:42:11 +0200

    Move Forward NXT

---
+++ MoveForwardNXT/src/com/nxtdomain/MoveForward.java
@@ -0,0 +1,12 @@
+package com.nxtdomain;
+
+import lejos.nxt.Motor;
+
+public class MoveForward {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Motor.A.forward();
+	}
+
+}

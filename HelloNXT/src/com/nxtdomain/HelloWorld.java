fa286f1efff930f211f163386bbdcc9d5621031d
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2014-05-06 17:28:43 +0200

    Hello NXT

---
+++ HelloNXT/src/com/nxtdomain/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.nxtdomain;
+
+import lejos.nxt.Motor;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Motor.A.forward();
+	}
+
+}

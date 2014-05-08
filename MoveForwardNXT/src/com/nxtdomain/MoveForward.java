602d23632bd6b9161c20efdcfac3495cd32ba17f
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2014-05-08 21:27:11 +0200

    Assignment 1 - Move Forward for few seconds

--- MoveForwardNXT/src/com/nxtdomain/MoveForward.java
+++ MoveForwardNXT/src/com/nxtdomain/MoveForward.java
@@ -1,12 +1,26 @@
 package com.nxtdomain;
 
 import lejos.nxt.Motor;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.util.Delay;
 
 public class MoveForward {
 
 	public static void main(String[] args) {
-        System.out.println("Hello World!");
+		LCD.drawString("Assignment 1", 0, 0);
+		Button.waitForAnyPress();
+		Delay.msDelay(1500);
+		LCD.clear();
+		LCD.drawString("MOVING FORWARD",0,0);
         Motor.A.forward();
+        Motor.C.forward();
+        Delay.msDelay(3500);
+        LCD.clear();
+        Motor.A.stop(true);
+        Motor.C.stop();
+        LCD.drawString("STOPPED MOVING",0,0);
+        Button.waitForAnyPress();
 	}
 
 }
